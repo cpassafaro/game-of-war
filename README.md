0c9bca30e829bfdb3aa50dcd832e539bb0bd380d
christinapassafaro <christinapassafaro@gmail.com>
2020-10-04 09:00:17 -0400

    fixed

---
+++ README.md
@@ -0,0 +1,50 @@
+# game-of-war
+Card Game: War
+Game of War
+
+Rules for the game of War:
+If you don't know how to play the card game war, read below through line 16. If you know the rules skip down to line 18 for the rules of playing on the computer!
+THE DEAL
+The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. Anyone may deal first. Each player places their stack of cards face down, in front of them.
+
+THE PLAY
+Each player turns up a card at the same time and the player with the higher card takes both cards and puts them, face down, on the bottom of his stack.
+
+If the cards are the same rank, it is War. Each player turns up one card face down and one card face up. The player with the higher cards takes both piles (six cards). If the turned-up cards are again the same rank, each player places another card face down and turns another card face up. The player with the higher card takes all 10 cards, and so on.
+
+HOW TO KEEP SCORE
+The game ends when one player has won all the cards.
+
+
+//These are the specifications of how to interact with the console to play the game!///
+
+player entry keys to know:
+war.playCard()
+war.compareCards()
+
+
+Set up players. Players have been set up in the app.js file. If you would like to change player names, you can do so on line 9 or 10.
+
+
+To start the game go the console on the webbrowser. When the browser is loaded the cards have been created via classes, shuffled, and dealt to two players. 
+
+Players can access or look at their decks via their profile. Type in player1.deck or player2.deck. Decks will be displayed.
+
+To start the game, each player must play a card. They can do this at the same time. Enter war.playCard() in the console. The game has begun
+
+The console logs which card each player has played.The cards now need to be compared. To do this the players need to run war.compareCards() in the console.If the players try to play another card before comparing the original cards, then they will be prompted to compare cards before continuting. The winner will be announced on the next line once cards are compared, or a tie will be announced. Each announcement will include how many cards each player has left.
+
+If there is no tie, then after the first round is played through, the players start the next round by running war.playCard() in the console. The players must enter war.compareCards() to compare cards and decide a winner.
+
+If there is a tie, then war is started and played through automatically, three cards are taken from the top of each of the players decks, and put in the winning pot. The 4th card is flipped over and played,  war.compareCards is run automatically and populates the results of the war.
+
+
+The games checks to see if the players have enough cards to play out a typical round before it begins. There is also another function that checks to see if the players have enough cards when a war declaration happens.
+
+
+If the players do not have enough cards to continue, the winner is announced and the game ends (because the functions won't work, because of there not being values to repopulate one of the players decks!)
+
+To start a new game refresh the page!
+
+
+
---
+++ app.js
@@ -0,0 +1,169 @@
+class Player {
+    constructor(name, deck){
+        this.name = name;
+        this.deck = deck;
+        this.playedCard = [];
+    }
+}
+//input players here
+const player1 = new Player('Teo', [], [])
+
+const player2 = new Player('Computer', [], [])
+//this class is called upon in the class of deck, it is the object that each card is built off of
+ class Card {
+     constructor(suit, rank, value){
+         this.suit = suit;
+         this.rank = rank;
+         this.value =value;
+     }
+ }
+
+
+ class Deck {
+     constructor(cards){
+         this.cards = cards;
+         this.createDeck()
+     }
+     createDeck(){
+        //internal information for cards and loops within function, not the way you try to access card properties outside
+        let deck =[];
+        let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        let value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14];
+        for(let i=0; i< suits.length; i++){
+            for(let j=0; j <cards.length; j++){
+                let card = new Card (suits[i], cards[j], value[j])
+                deck.push(card)
+            }
+        }
+        this.cards = deck;
+     }
+
+     shuffleDeck(){
+        var ctr = this.cards.length, temp, index;
+
+        // While there are elements in the array
+            while (ctr > 0) {
+        // Pick a random index
+                index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+                ctr--;
+        // And swap the last element with it
+                temp = this.cards[ctr];
+                this.cards[ctr] = this.cards[index];
+                this.cards[index] = temp;
+            }
+            return this.cards;
+     }
+
+     dealCards(){
+         //not sure why it needs to loop twice to deal out the correct amount of cards
+        for(let i = 0; i < this.cards.length; i++){
+            for(let j=0; j <this.cards.length; j++){
+                // player1.this.deck.push(cards.shift());
+                // player2.this.deck.push(cards.shift());
+                let firstDeck = this.cards.shift(0,25);
+                let secondDeck = this.cards.shift(26,51);
+                // console.log(halfDeck)
+                player1.deck.push(firstDeck);
+                player2.deck.push(secondDeck)
+                
+            }
+                // player1.deck.push(cards.shift());
+        
+            }
+            return (player1.deck, player2.deck) 
+        }
+    }
+
+class WarGames {
+    constructor(round){
+        this.playedCards =[];
+        this.winnerPot =[];
+        this.round = round;
+    }
+    playCard(){
+        //couldn't get the push command to work without saving the players cards in another variable
+        // let player1PlayedCard = player1.playedCard;
+        // let player2PlayedCard = player2.playedCard;
+        if(player1.playedCard.length == 1 && player2.playedCard.length == 1){
+            alert(`Compare the played cards before continuing!`)
+        }else{
+
+        let player1PlayedCard = player1.playedCard;
+        let player2PlayedCard = player2.playedCard;
+            
+        player1PlayedCard.push((player1.deck.shift()));
+        player2PlayedCard.push((player2.deck.shift()));
+        console.log(`Round ${this.round++} :${player1.name} played the ${player1PlayedCard[0].rank} of ${player1PlayedCard[0].suit} and ${player2.name} played the ${player2PlayedCard[0].rank} of ${player2PlayedCard[0].suit}.`)
+        // this.compareCards();
+        }
+    }
+    compareCards(){
+        //to get the cards to push into the array, without being saved in another array I had to declare the index of each one
+        if(player1.playedCard[0].value > player2.playedCard[0].value){
+            this.winnerPot.push(player1.playedCard[0], player2.playedCard[0])
+            player1.deck.push(...this.winnerPot);
+            player1.playedCard = [];
+            player2.playedCard = [];
+            this.winnerPot = [];
+            console.log(`${player1.name} won! ${player1.name} has ${player1.deck.length} cards left, and ${player2.name} has ${player2.deck.length} cards left`) 
+            this.cardCheckForNormalRound()      
+        }else if (player2.playedCard[0].value > player1.playedCard[0].value){
+            this.winnerPot.push(player1.playedCard[0], player2.playedCard[0])
+            player2.deck.push(...this.winnerPot)
+            player1.playedCard = [];
+            player2.playedCard = [];
+            this.winnerPot =[];
+            console.log(`${player2.name} won! ${player2.name} has ${player2.deck.length} cards left, and ${player1.name} has ${player1.deck.length} cards left`)
+            this.cardCheckForNormalRound()
+        }else{
+            this.winnerPot.push(player1.playedCard[0], player2.playedCard[0])
+            player1.playedCard = [];
+            player2.playedCard = [];
+            console.log(`its a tie`)
+            this.cardCheckForWar()
+            this.declareWar()
+        }      
+    }
+    declareWar(){
+        //push three cards from each players deck into the winner pot
+        let playerOneArr = player1.deck.splice(0,3);
+        let playerTwoArr = player2.deck.splice(0,3);
+        this.winnerPot.push(...playerOneArr);
+        this.winnerPot.push(...playerTwoArr);
+        //play a card from each players hand
+        war.playCard();
+        war.compareCards();
+    }
+    cardCheckForWar(){
+        //players must have at least 4 cards left for a war/tie, this checks that, if they don't have enough cards they lose!
+        if(player1.deck < 4){
+            alert(`${player1.name} is the winner!`)
+        }else if (player2.deck < 4){
+            alert(`${player2.deck} is the winner!`)
+        }    
+    }
+    cardCheckForNormalRound(){
+        //checking to see if players are out of cards completely
+        if(player1.deck == 0 ){
+            console.log(`${player2.name} won! ${player1.name} doesn't have any more cards`);
+        }else if (player2.deck == 0){
+            console.log(`${player1.name} won! ${player2.name} doesn't have any more cards`)
+
+        }
+    }
+}
+
+
+
+
+//these are decarled here so that the game is ready to go as soon as the page is reloaded
+//need instances of classes to call our functions to start game
+const deck = new Deck;
+const war = new WarGames(1);
+
+//shuffles deck on page refresh
+deck.shuffleDeck()
+//deals cards out to players when page refreshed
+deck.dealCards()
